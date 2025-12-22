d3cbc992ceea6c356bb2d1e7697969087007d926
QuangLe1710 <160602994+QuangLe1710@users.noreply.github.com>
2025-12-22 16:30:06 +0700

    finish 22/12/2025

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,7 @@ import * as utils from './scripts/Util.jsx'
 
 function App() {
 
-  const hobbies = ["Cooking", "Reading", "Working"]
-
-  const newHobbies = ['Coding'];
-
-  const lastestHobbies = [...hobbies , ...newHobbies];
-
-  console.log("Hobbies : " , hobbies);
-  console.log("New Hobbies : " , newHobbies);
-  console.log("Lastest Hobbies : " , lastestHobbies);
-
-  const obj = {
-    username : "quang",
-    age: 21
-  }
-
-  const newObj = {
-    rank : "challenge"
-  }
-
-  const lastestObj = {...obj , ...newObj};
-
-  console.log("Obj : " , obj);
-
-  console.log("New Obj : " , newObj);
-
-  console.log("Lastest Obj : " , lastestObj);
-
+  
 
   return (
     <>
