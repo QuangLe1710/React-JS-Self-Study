79bf7f06ed0a2f65dbe6a4261b586cce3f6227a7
QuangLe1710 <160602994+QuangLe1710@users.noreply.github.com>
2025-12-22 14:33:48 +0700

    The spread operator

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,33 @@ import * as utils from './scripts/Util.jsx'
 
 function App() {
 
-  const [count, setCount] = useState(5)
+  const hobbies = ["Cooking", "Reading", "Working"]
+
+  const newHobbies = ['Coding'];
+
+  const lastestHobbies = [...hobbies , ...newHobbies];
+
+  console.log("Hobbies : " , hobbies);
+  console.log("New Hobbies : " , newHobbies);
+  console.log("Lastest Hobbies : " , lastestHobbies);
+
+  const obj = {
+    username : "quang",
+    age: 21
+  }
+
+  const newObj = {
+    rank : "challenge"
+  }
+
+  const lastestObj = {...obj , ...newObj};
+
+  console.log("Obj : " , obj);
+
+  console.log("New Obj : " , newObj);
+
+  console.log("Lastest Obj : " , lastestObj);
+
 
   return (
     <>
